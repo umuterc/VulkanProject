301126725db97d85122c6fde420f7019da15dcb8
Umut Ercan <umuterc123@gmail.com>
2023-04-27 15:15:21 +0200

    Fix auto shader compile

--- assets/shaders/compile_mac.sh
+++ assets/shaders/compile_mac.sh
@@ -1,3 +1,22 @@
-/Users/umutercan/VulkanSDK/1.3.239.0/macOS/bin/glslc shader.vert -o vert.spv
-/Users/umutercan/VulkanSDK/1.3.239.0/macOS/bin/glslc shader.frag -o frag.spv
-./
+#!/bin/sh
+
+# Set execute permission on this script
+chmod +x "$0"
+
+# Compile vertex shader
+/Users/umutercan/VulkanSDK/1.3.239.0/macOS/bin/glslc shader.vert -o vert.spv || {
+    echo "Error: Failed to compile vertex shader"
+    exit 1
+}
+
+# Compile fragment shader
+/Users/umutercan/VulkanSDK/1.3.239.0/macOS/bin/glslc shader.frag -o frag.spv || {
+    echo "Error: Failed to compile fragment shader"
+    exit 1
+}
+
+# Print directory of compiled shader program binary
+echo "Compiled shader program binary located in $(pwd)"
+
+# Success message
+echo "Shaders compiled successfully"
